bc80d484944c6434b19675eee706692b2cc89bc3
Devon Terrell <zeugmasyllepsis@gmail.com>
2014-04-20 22:39:06 -0400

    Pushing section selection changes.

--- static/js/app.js
+++ static/js/app.js
@@ -7,14 +7,28 @@ hugryApp.value('navgridTemplate', 'static/js/app/navgrid/navgrid.html');
 var hugryApp = angular.module('hugryApp');
 
 hugryApp.controller('navgrid', function($scope, navgridTemplate){
-    var sections = ['Profile', 'Music', 'Sheet Music', 'Equipment', 'Resume', 'Contact'];
+    var sections = ['Profile', 'Music', 'Scores', 'Equipment', 'Resume', 'Contact'];
     var sectionIcons = ['fa-info-circle', 'fa-play-circle', 'fa-music', 'fa-desktop fa-microphone', 'fa-file-text', 'fa-envelope'];
+    var sectionDetails = ['info', 'music', 'scores', 'equipment', 'resume', 'contact'];
 
-    function Section(sectionName, iconName){
+    function Section(sectionName, iconName, sectionDetailsTemplate){
         this.SectionName = sectionName;
         this.SectionIcon = iconName;
+        this.SectionDetails = sectionDetailsTemplate;
     }
 
+    var defaultSection = new Section("Home", "", "site/base");
+
+    $scope.setSelectedSection = function(section){
+        if (section){
+            $scope.selectedSection = section;
+        } else {
+            $scope.selectedSection = defaultSection;
+        }
+    };
+
+    $scope.selectedSection = defaultSection;
+
     $scope.navgridTemplate = navgridTemplate;
-    $scope.sections = _.map(_.zip(sections, sectionIcons), function(d){return new Section(d[0], d[1]);});
+    $scope.sections = _.map(_.zip(sections, sectionIcons, sectionDetails), function(d){return new Section(d[0], d[1], 'site/' + d[2]);});
 });
