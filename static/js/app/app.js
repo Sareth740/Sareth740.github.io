8ea5d15712cd1ee3d728c60d2e164453d7589f03
Devon Terrell <zeugmasyllepsis@gmail.com>
2014-04-21 22:35:53 -0400

    Animation refinements.

--- static/js/app/app.js
+++ static/js/app/app.js
@@ -3,20 +3,25 @@ var hugryApp = angular.module('hugryApp', []);
 hugryApp.value('navgridTemplate', 'static/js/app/navgrid/navgrid.html');
 
 hugryApp.controller('hugryController', function($scope, $timeout){
-    $timeout(function(){
-        $scope.finishedLoading = true;
-    }, 1000);
 
     $timeout(function(){
-        $scope.finishedFadeIn = true;
+        $scope.finishedLoading = true;
+    }, 1000).then(function(){
+        $timeout(function(){
+            $scope.finishedEnteringScriptBlock = true;
+        }, 700);
+    }).then(function(){
         $timeout(function(){
             $scope.finishedAnimatingScriptBlock = true;
-            $timeout(function(){
-                $scope.finishedEnteringBlockGrid = true;
-                $timeout(function(){
-                    $scope.finishedAnimatingGrid = true;
-                }, 500);
-            }, 800);
-        }, 1000);
-    }, 2000);
+        }, 1700);
+    }).then(function(){
+        $timeout(function(){
+            $scope.finishedEnteringGrid = true;
+        }, 2700);
+    }).then(function(){
+        $timeout(function(){
+            $scope.finishedAnimatingGrid = true;
+        }, 3500);
+    });
+
 });
