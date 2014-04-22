192a0592ab495ed0c19cc2c92373726e6fcd1544
Devon Terrell <zeugmasyllepsis@gmail.com>
2014-04-21 21:52:26 -0400

    Animation refinement.

--- static/js/app/app.js
+++ static/js/app/app.js
@@ -5,15 +5,18 @@ hugryApp.value('navgridTemplate', 'static/js/app/navgrid/navgrid.html');
 hugryApp.controller('hugryController', function($scope, $timeout){
     $timeout(function(){
         $scope.finishedLoading = true;
-    }, 200);
+    }, 1000);
 
     $timeout(function(){
         $scope.finishedFadeIn = true;
         $timeout(function(){
             $scope.finishedAnimatingScriptBlock = true;
             $timeout(function(){
-                $scope.finishedAnimatingGrid = true;
-            }, 500);
+                $scope.finishedEnteringBlockGrid = true;
+                $timeout(function(){
+                    $scope.finishedAnimatingGrid = true;
+                }, 500);
+            }, 800);
         }, 1000);
-    }, 1000);
+    }, 2000);
 });
